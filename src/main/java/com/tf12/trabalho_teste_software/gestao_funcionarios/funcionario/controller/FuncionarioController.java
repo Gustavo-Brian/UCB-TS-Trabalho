e91c024b3fbb7a2a57b81203f66559344326c5b3
John <johnrclm5@gmail.com>
2025-03-23 14:58:01 -0300

    testando

--- src/main/java/com/tf12/trabalho_teste_software/gestao_funcionarios/funcionario/controller/FuncionarioController.java
+++ src/main/java/com/tf12/trabalho_teste_software/gestao_funcionarios/funcionario/controller/FuncionarioController.java
@@ -1,5 +1,6 @@
 package com.tf12.trabalho_teste_software.gestao_funcionarios.funcionario.controller;
 
+//teste
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,58 +23,56 @@ import com.tf12.trabalho_teste_software.gestao_funcionarios.funcionario.service.
 @RestController
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
-	
+
 	private final FuncionarioService service;
-	
+
 	public FuncionarioController(FuncionarioService service) {
 		super();
 		this.service = service;
 	}
-	
+
 	@PostMapping("/criarFuncionario")
 	public ResponseEntity<Boolean> criarFuncionario(@RequestBody FuncionarioRequest dto) {
 		return ResponseEntity.status(201).body(service.criarFuncionario(dto));
 	}
-	
+
 	@GetMapping("/retornarFuncionario/{id}")
 	public ResponseEntity<FuncionarioResponse> retornarFuncionario(@PathVariable Long id) {
 		FuncionarioResponse dto = service.retornarFuncionario(id);
-	
+
 		return (dto == null) ? ResponseEntity.notFound().build() : ResponseEntity.status(200).body(dto);
 	}
-	
+
 	@GetMapping("/listarFuncionarios")
 	public ResponseEntity<FuncionariosResponse> listarFuncionarios() {
 		return ResponseEntity.status(200).body(service.retornarLista());
 	}
-	
+
 	@GetMapping("/listarFuncionariosAtivos")
 	public ResponseEntity<FuncionariosAtivosResponse> listarFuncionarioAtivos() {
 		return ResponseEntity.status(200).body(service.retornarAtivos());
 	}
-	
+
 	@GetMapping("/listarFuncionariosInativos")
 	public ResponseEntity<FuncionariosInativosResponse> listarFuncionariosInativos() {
 		return ResponseEntity.status(200).body(service.retornarInativos());
 	}
-	
+
 	@PutMapping("/desativarFuncionario/{id}")
-	public ResponseEntity<String> desativar(@PathVariable Long id){
+	public ResponseEntity<String> desativar(@PathVariable Long id) {
 		service.desativar(id);
 		return ResponseEntity.status(200).body("Funcionario Desativado com sucesso");
 	}
-	
+
 	@PutMapping("/ativarFuncionario/{id}")
-	public ResponseEntity<String> ativar(@PathVariable Long id){
+	public ResponseEntity<String> ativar(@PathVariable Long id) {
 		service.ativar(id);
 		return ResponseEntity.status(200).body("Funcionario ativado com sucesso");
 	}
-	
+
 	@DeleteMapping("/deletarFuncionario/{id}")
 	public ResponseEntity<Boolean> deletar(@PathVariable Long id) {
 		return ResponseEntity.status(200).body(service.deletarFuncionario(id));
 	}
-	
-	
-	
+
 }
